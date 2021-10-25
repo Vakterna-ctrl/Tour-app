b1560f03cd9432769f0871d13643cbafd51b14b1
chaukhadoan <kha.doanchau@gmail.com>
2021-10-25 23:23:12 +0200

    handling errors

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 const mongoose = require('mongoose');
+
+process.on('uncaughtException', err => {
+  console.log('UNHANDLED REJECTION! Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 require('dotenv').config({ path: `${__dirname}/config.env` });
 const app = require('./app');
 
@@ -20,6 +27,18 @@ mongoose
   });
 
 const port = process.env.port || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
+
+// Add this to take care of the edge case asynchronous rejections
+process.on('unhandledRejection', err => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION! Shutting down...');
+  // Give server time to finish the request that are still pending before shutting down
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+// Handling Uncaught exception
