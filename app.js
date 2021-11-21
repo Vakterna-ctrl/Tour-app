bace1c505c9db6d8994489176488dd084f0d0c8d
chaukhadoan <kha.doanchau@gmail.com>
2021-11-21 19:07:35 +0100

    stripe fix

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ app.use('/api', limiter);
 
 app.post(
   '/webhook-checkout',
-  express.raw({ type: 'application/json' }),
+  express.raw({ type: '*/*' }),
   bookingController.webhookCheckout
 );
 
