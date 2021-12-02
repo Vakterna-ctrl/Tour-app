47af117eeb80bd9190cc3aa262fb02986ffc3450
Chau Kha Doan <kha.doanchau@gmail.com>
2021-12-02 02:18:56 +0100

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -50,7 +50,7 @@ const userSchema = new mongoose.Schema({
 
 userSchema.pre('save', async function(next) {
   // Only run this function if password was actually modified
-  if (!this.isModified('password')) return;
+  if (!this.isModified('password')) return next();
   // Hash the password with the cost of 12
   this.password = await bcrypt.hash(this.password, 12);
   // Delete passwordConfirm field
